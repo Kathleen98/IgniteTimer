6339800933f0c533cd4320688c4dc3861568d848
Kathleen <99849068+Kathleen98@users.noreply.github.com>
2025-02-09 18:51:26 +0000

    feat: Defined route layout

--- src/components/Router/index.tsx
+++ src/components/Router/index.tsx
@@ -1,14 +1,17 @@
-import {Routes, Route} from 'react-router-dom'
-import Home from '../../pages/Homes';
-import History from '../../pages/History';
+import { Routes, Route } from "react-router-dom";
+import Home from "../../pages/Homes";
+import History from "../../pages/History";
+import DefaultLayout from "../../layouts/DeaultLayout";
 
 const Router = () => {
-  return(
+  return (
     <Routes>
+      <Route path={"/"} element={<DefaultLayout />}>
         <Route path={"/"} element={<Home />} />
         <Route path={"/History"} element={<History />} />
+      </Route>
     </Routes>
-  )
+  );
 };
 
 export default Router;
