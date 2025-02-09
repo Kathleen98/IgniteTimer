21b853f81300fa600ba5caa367e1e64971fd114a
Kathleen <99849068+Kathleen98@users.noreply.github.com>
2025-02-09 16:17:35 +0000

    feat: defining global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { ButtonContainer } from "./Button.styles";
+import { GlobalStyle } from "./styles/themes/global";
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <ButtonContainer>Enviar</ButtonContainer>
+      <GlobalStyle />
     </ThemeProvider>
   );
 }
