573cf808fdc7104c7b04030c301b5816eb9d893d
Kathleen <99849068+Kathleen98@users.noreply.github.com>
2025-02-15 00:49:04 +0000

    feat: styling status section, fixing global style hex value

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from "./styles";
+import { HistoryContainer, HistoryList, Status } from "./styles";
 
 const History = () => {
   return (
@@ -20,25 +20,25 @@ const History = () => {
               <td>Tarefa</td>
               <td>20 min</td>
               <td>Há dois meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 min</td>
               <td>Há dois meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 min</td>
               <td>Há dois meses</td>
-              <td>Concluído</td>
-            </tr>
-            <tr>
-              <td>Tarefa</td>
-              <td>20 min</td>
-              <td>Há dois meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
             </tr>
           </tbody>
         </table>
