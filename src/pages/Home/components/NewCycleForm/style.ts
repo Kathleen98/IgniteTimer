5b2427199b649a42f4e3edd12043f8354cf76222
Kathleen Santos <kathleen.santos@csacademy.com.br>
2025-02-24 21:15:34 -0300

    feat:adding context to the home page

---
+++ src/pages/Home/components/NewCycleForm/style.ts
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+export const FormContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8.5rem;
+  color: ${(props) => props.theme["gray-100"]};
+  font-size: 1.125rem;
+  font-weight: bold;
+  flex-wrap: wrap;
+`;
+
+const baseInput = styled.input`
+  background: transparent;
+  height: 2.5rem;
+  border: 0;
+  border-bottom: 2px solid ${(props) => props.theme["gray-500"]};
+  font-weight: bold;
+  font-size: 0.75rem;
+  padding: 0 0.5rem;
+  color: ${(props) => props.theme["gray-100"]};
+
+  &:focus {
+    box-shadow: none;
+    border-color: ${(props) => props.theme["green-500"]};
+  }
+
+  &::placeholder {
+    color: ${(props) => props.theme["gray-500"]};
+  }
+`;
+
+export const TasksInput = styled(baseInput)`
+  flex: 1;
+
+  &::-webkit-calendar-picker-indicator {
+    display: none !important;
+  }
+`;
+
+export const MinutesAmountInput = styled(baseInput)`
+  width: 3rem;
+`;
